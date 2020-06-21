391e252874f00041c910a238da2ec289d33fdf3c
Annie Vogel Ciernia <a.ciernia@gmail.com>
2020-06-20 19:20:26 -0700

    Add files via upload

---
+++ pretrim_fastqc.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+#SBATCH -c 1
+#SBATCH --mem-per-cpu=1000
+#SBATCH --job-name=PreTrimFastqc
+#SBATCH --output=PreTrimFastqc.out
+#SBATCH --time=2:00:00
+
+#######################################################################################
+#fastqc and then combine reports into one html and save to outputs folder
+
+#######################################################################################
+mkdir -p /output/pretrim
+
+for sample in `cat SRR_Acc_List.txt`
+do
+
+echo ${sample} "starting"
+
+fastqc /alder/data/cbh/ciernia-data/HDAC1_2_ChIPseq/SRA/${sample}_1.fastq.gz --outdir output/pretrim
+
+fastqc /alder/data/cbh/ciernia-data/HDAC1_2_ChIPseq/SRA/${sample}_2.fastq.gz --outdir output/pretrim
+
+echo ${sample} "finished"
+
+
+done
+
+
+
+#######################################################################################
+#combine
+#######################################################################################
+
+multiqc output/pretrim --filename output/PretrimFastQC_multiqc_report.html --ignore-samples Undetermined* --interactive
