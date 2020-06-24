524570be1913d63190a878fafe206a4c5438ee63
Justin Jao <47490085+justinjao@users.noreply.github.com>
2020-06-24 10:24:39 -0700

    updated script to fix the directory issue

--- SRRpull.sh
+++ SRRpull.sh
@@ -10,23 +10,25 @@
 #HDAC1&2 CHIPSEQ analysis
 #pull SRA files from GEO: https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE107436
 #######################################################################################
-# run from home directory
+# run from HDAC1_2_ChIPseq directory
 #puts data in shared data file for the experiment
 #######################################################################################
-mkdir -p /alder/data/cbh/ciernia-data/HDAC1_2_ChIPseq/SRA/
+mkdir -p SRA/
 
 for sample in `cat SRR_Acc_List.txt`
 do
 
+cd ~/HDAC1_2_ChIPseq/SRA/
+
 echo ${sample} "starting SRR pull"
-prefetch /alder/data/cbh/ciernia-data/HDAC1_2_ChIPseq/SRA/${sample}
+prefetch ${sample}
 
 #######################################################################################
 #paired end sra > Fastq.gz
 #######################################################################################
 echo ${sample} "starting dump"
 
-fastq-dump --origfmt --split-files --gzip /alder/data/cbh/ciernia-data/HDAC1_2_ChIPseq/SRA/${sample}
+fastq-dump --origfmt --split-files --gzip ${sample}
 
 echo ${sample} "done"
 #######################################################################################
