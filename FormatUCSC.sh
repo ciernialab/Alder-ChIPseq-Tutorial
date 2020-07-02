2a08607f359d45d5a4704214af36bafd7c6ed5be
Annie Vogel Ciernia <a.ciernia@gmail.com>
2020-07-02 11:45:49 -0700

    change MT to M

--- FormatUCSC.sh
+++ FormatUCSC.sh
@@ -8,57 +8,74 @@
 
 #######################################################################################
 #make into ucsc format
+#sed -i 's/old-text/new-text/g' input.txt
 #######################################################################################
 #skip the first line, then add "chr" to each chromosome
 sed -i "1n; s/^/chr/" UCSCbrowsertracks/HDAC12KO_H3K27ac_Repl1.bedGraph
+sed -i "1n; s/MT/M/g" UCSCbrowsertracks/HDAC12KO_H3K27ac_Repl1.bedGraph
 
 #skip the first line, then add "chr" to each chromosome
 sed -i "1n; s/^/chr/" UCSCbrowsertracks/HDAC12KO_H3K27ac_Repl2.bedGraph
+sed -i "1n; s/MT/M/g" UCSCbrowsertracks/HDAC12KO_H3K27ac_Repl2.bedGraph
 
 #skip the first line, then add "chr" to each chromosome
 sed -i "1n; s/^/chr/" UCSCbrowsertracks/HDAC12KO_H3K27ac_Repl3.bedGraph
+sed -i "1n; s/MT/M/g" UCSCbrowsertracks/HDAC12KO_H3K27ac_Repl3.bedGraph
 
 #skip the first line, then add "chr" to each chromosome
 sed -i "1n; s/^/chr/" UCSCbrowsertracks/HDAC12KO_Input_Repl1.bedGraph
+sed -i "1n; s/MT/M/g" UCSCbrowsertracks/HDAC12KO_Input_Repl1.bedGraph
 
 #skip the first line, then add "chr" to each chromosome
 sed -i "1n; s/^/chr/" UCSCbrowsertracks/HDAC12KO_Input_Repl2.bedGraph
+sed -i "1n; s/MT/M/g" UCSCbrowsertracks/HDAC12KO_Input_Repl2.bedGraph
 
 #skip the first line, then add "chr" to each chromosome
 sed -i "1n; s/^/chr/" UCSCbrowsertracks/WT_H3K27ac_Repl1.bedGraph
+sed -i "1n; s/MT/M/g" UCSCbrowsertracks/WT_H3K27ac_Repl1.bedGraph
 
 #skip the first line, then add "chr" to each chromosome
 sed -i "1n; s/^/chr/" UCSCbrowsertracks/WT_H3K27ac_Repl2.bedGraph
+sed -i "1n; s/MT/M/g" UCSCbrowsertracks/WT_H3K27ac_Repl2.bedGraph
 
 #skip the first line, then add "chr" to each chromosome
 sed -i "1n; s/^/chr/" UCSCbrowsertracks/WT_Input_Repl1.bedGraph
+sed -i "1n; s/MT/M/g" UCSCbrowsertracks/WT_Input_Repl1.bedGraph
 
 #skip the first line, then add "chr" to each chromosome
 sed -i "1n; s/^/chr/" UCSCbrowsertracks/WT_Input_Repl2.bedGraph
+sed -i "1n; s/MT/M/g" UCSCbrowsertracks/WT_Input_Repl2.bedGraph
 
 #skip the first line, then add "chr" to each chromosome
 sed -i "1n; s/^/chr/" UCSCbrowsertracks/WT_Input_Repl3.bedGraph
+sed -i "1n; s/MT/M/g" UCSCbrowsertracks/WT_Input_Repl3.bedGraph
 
 #skip the first line, then add "chr" to each chromosome
 sed -i "1n; s/^/chr/" UCSCbrowsertracks/WT_Input_Repl4.bedGraph
+sed -i "1n; s/MT/M/g" UCSCbrowsertracks/WT_Input_Repl4.bedGraph
 
 #skip the first line, then add "chr" to each chromosome
 sed -i "1n; s/^/chr/" UCSCbrowsertracks/WT_Input_Repl5.bedGraph
+sed -i "1n; s/MT/M/g" UCSCbrowsertracks/WT_Input_Repl5.bedGraph
 
 #skip the first line, then add "chr" to each chromosome
 sed -i "1n; s/^/chr/" UCSCbrowsertracks/HDAC12KO_H3K9ac_Repl1.bedGraph
+sed -i "1n; s/MT/M/g" UCSCbrowsertracks/HDAC12KO_H3K9ac_Repl1.bedGraph
 
 #skip the first line, then add "chr" to each chromosome
 sed -i "1n; s/^/chr/" UCSCbrowsertracks/HDAC12KO_H3K9ac_Repl2.bedGraph
+sed -i "1n; s/MT/M/g" UCSCbrowsertracks/HDAC12KO_H3K9ac_Repl2.bedGraph
 
 #skip the first line, then add "chr" to each chromosome
 sed -i "1n; s/^/chr/" UCSCbrowsertracks/WT_H3K9ac_Repl1.bedGraph
+sed -i "1n; s/MT/M/g" UCSCbrowsertracks/WT_H3K9ac_Repl1.bedGraph
 
 #skip the first line, then add "chr" to each chromosome
 sed -i "1n; s/^/chr/" UCSCbrowsertracks/WT_H3K9ac_Repl2.bedGraph
+sed -i "1n; s/MT/M/g" UCSCbrowsertracks/WT_H3K9ac_Repl2.bedGraph
 
 #skip the first line, then add "chr" to each chromosome
 sed -i "1n; s/^/chr/" UCSCbrowsertracks/WT_H3K9ac_Repl3.bedGraph
+sed -i "1n; s/MT/M/g" UCSCbrowsertracks/WT_H3K9ac_Repl3.bedGraph
 
-#zip files
 gzip UCSCbrowsertracks/*.bedGraph
