638f54db30284d0e7a51d6c867a93d16e07656f3
Justin Jao <47490085+justinjao@users.noreply.github.com>
2020-09-03 21:15:02 -0700

    added missingDataAsZero, and bwr color scheme

--- Geneplots_deeptools.sh
+++ Geneplots_deeptools.sh
@@ -12,27 +12,27 @@
 ########################################################################################
 #H3K27ac
 ##########################################################################################
-computeMatrix reference-point --referencePoint TSS -b 1000 -a 500 -R mm10.refseq.bed -S UCSCbrowsertracks/HDAC12KO_H3K27ac_Repl1.bw UCSCbrowsertracks/HDAC12KO_H3K27ac_Repl2.bw UCSCbrowsertracks/HDAC12KO_H3K27ac_Repl3.bw UCSCbrowsertracks/WT_H3K27ac_Repl1.bw UCSCbrowsertracks/WT_H3K27ac_Repl2.bw UCSCbrowsertracks/HDAC12KO_Input_Repl1.bw UCSCbrowsertracks/HDAC12KO_Input_Repl2.bw UCSCbrowsertracks/WT_Input_Repl1.bw UCSCbrowsertracks/WT_Input_Repl2.bw UCSCbrowsertracks/WT_Input_Repl3.bw UCSCbrowsertracks/WT_Input_Repl4.bw UCSCbrowsertracks/WT_Input_Repl5.bw -p 20 -o Deeptools/mouseTSS.H3K27ac.tab.gz --skipZeros --binSize 10 --outFileSortedRegions Deeptools/H3K27acTSS_out.bed 
+computeMatrix reference-point --referencePoint TSS -b 1000 -a 500 -R mm10.refseq.bed -S UCSCbrowsertracks/HDAC12KO_H3K27ac_Repl1.bw UCSCbrowsertracks/HDAC12KO_H3K27ac_Repl2.bw UCSCbrowsertracks/HDAC12KO_H3K27ac_Repl3.bw UCSCbrowsertracks/WT_H3K27ac_Repl1.bw UCSCbrowsertracks/WT_H3K27ac_Repl2.bw UCSCbrowsertracks/HDAC12KO_Input_Repl1.bw UCSCbrowsertracks/HDAC12KO_Input_Repl2.bw UCSCbrowsertracks/WT_Input_Repl1.bw UCSCbrowsertracks/WT_Input_Repl2.bw UCSCbrowsertracks/WT_Input_Repl3.bw UCSCbrowsertracks/WT_Input_Repl4.bw UCSCbrowsertracks/WT_Input_Repl5.bw -p 20 -o Deeptools/mouseTSS.H3K27ac.tab.gz --skipZeros --missingDataAsZero --binSize 10 --outFileSortedRegions Deeptools/H3K27acTSS_out.bed
 
 # plot
-plotProfile -m Deeptools/mouseTSS.H3K27ac.tab.gz  --numPlotsPerRow 2 --regionsLabel "Mouse RefSeq Genes" --plotFileFormat "pdf" -out Deeptools/profile_H3K27ac.pdf --averageType mean --samplesLabel "KO" "KO" "KO" "WT" "WT" "KOInput" "KOInput" "WTInput" "WTInput" "WTInput" "WTInput" "WTInput" 
+plotProfile -m Deeptools/mouseTSS.H3K27ac.tab.gz  --numPlotsPerRow 2 --regionsLabel "Mouse RefSeq Genes" --plotFileFormat "pdf" -out Deeptools/profile_H3K27ac.pdf --averageType mean --samplesLabel "KO" "KO" "KO" "WT" "WT" "KOInput" "KOInput" "WTInput" "WTInput" "WTInput" "WTInput" "WTInput"
 
 
 # with legend media ymax chosen based on initial graphs
-plotHeatmap -m Deeptools/mouseTSS.H3K27ac.tab.gz --regionsLabel "Mouse Refeq Genes" --plotFileFormat "pdf" --samplesLabel "KO" "KO" "KO" "WT" "WT" "KOInput" "KOInput" "WTInput" "WTInput" "WTInput" "WTInput" "WTInput" -out Deeptools/Heatmap.mouseTSS.H3K27ac.pdf --averageType mean --perGroup --colorMap RdBu --averageTypeSummaryPlot mean --legendLocation upper-right --heatmapWidth 12
+plotHeatmap -m Deeptools/mouseTSS.H3K27ac.tab.gz --regionsLabel "Mouse Refeq Genes" --plotFileFormat "pdf" --samplesLabel "KO" "KO" "KO" "WT" "WT" "KOInput" "KOInput" "WTInput" "WTInput" "WTInput" "WTInput" "WTInput" -out Deeptools/Heatmap.mouseTSS.H3K27ac.pdf --averageType mean --perGroup --colorMap bwr --averageTypeSummaryPlot mean --legendLocation upper-right --heatmapWidth 12
 
 ############################################################################################
 
 ########################################################################################
 #H3K9ac
 ##########################################################################################
-computeMatrix reference-point --referencePoint TSS -b 1000 -a 500 -R mm10.refseq.bed -S UCSCbrowsertracks/HDAC12KO_H3K9ac_Repl1.bw UCSCbrowsertracks/HDAC12KO_H3K9ac_Repl2.bw UCSCbrowsertracks/WT_H3K9ac_Repl1.bw UCSCbrowsertracks/WT_H3K9ac_Repl2.bw UCSCbrowsertracks/WT_H3K9ac_Repl3.bw UCSCbrowsertracks/HDAC12KO_Input_Repl1.bw UCSCbrowsertracks/HDAC12KO_Input_Repl2.bw UCSCbrowsertracks/WT_Input_Repl1.bw UCSCbrowsertracks/WT_Input_Repl2.bw UCSCbrowsertracks/WT_Input_Repl3.bw UCSCbrowsertracks/WT_Input_Repl4.bw UCSCbrowsertracks/WT_Input_Repl5.bw -p 20 -o Deeptools/mouseTSS.H3K9ac.tab.gz --skipZeros --binSize 10 --outFileSortedRegions Deeptools/H3K9acTSS_out.bed 
+computeMatrix reference-point --referencePoint TSS -b 1000 -a 500 -R mm10.refseq.bed -S UCSCbrowsertracks/HDAC12KO_H3K9ac_Repl1.bw UCSCbrowsertracks/HDAC12KO_H3K9ac_Repl2.bw UCSCbrowsertracks/WT_H3K9ac_Repl1.bw UCSCbrowsertracks/WT_H3K9ac_Repl2.bw UCSCbrowsertracks/WT_H3K9ac_Repl3.bw UCSCbrowsertracks/HDAC12KO_Input_Repl1.bw UCSCbrowsertracks/HDAC12KO_Input_Repl2.bw UCSCbrowsertracks/WT_Input_Repl1.bw UCSCbrowsertracks/WT_Input_Repl2.bw UCSCbrowsertracks/WT_Input_Repl3.bw UCSCbrowsertracks/WT_Input_Repl4.bw UCSCbrowsertracks/WT_Input_Repl5.bw -p 20 -o Deeptools/mouseTSS.H3K9ac.tab.gz --skipZeros --missingDataAsZero --binSize 10 --outFileSortedRegions Deeptools/H3K9acTSS_out.bed
 
 # plot
-plotProfile -m Deeptools/mouseTSS.H3K9ac.tab.gz  --numPlotsPerRow 2 --regionsLabel "Mouse RefSeq Genes" --plotFileFormat "pdf" -out Deeptools/profile_H3K9ac.pdf --averageType mean --samplesLabel "KO" "KO" "WT" "WT" "WT" "KOInput" "KOInput" "WTInput" "WTInput" "WTInput" "WTInput" "WTInput" 
+plotProfile -m Deeptools/mouseTSS.H3K9ac.tab.gz  --numPlotsPerRow 2 --regionsLabel "Mouse RefSeq Genes" --plotFileFormat "pdf" -out Deeptools/profile_H3K9ac.pdf --averageType mean --samplesLabel "KO" "KO" "WT" "WT" "WT" "KOInput" "KOInput" "WTInput" "WTInput" "WTInput" "WTInput" "WTInput"
 
 
 # with legend media ymax chosen based on initial graphs
-plotHeatmap -m Deeptools/mouseTSS.H3K9ac.tab.gz --regionsLabel "Mouse Refeq Genes" --plotFileFormat "pdf" --samplesLabel "KO" "KO" "WT" "WT" "WT" "KOInput" "KOInput" "WTInput" "WTInput" "WTInput" "WTInput" "WTInput" -out Deeptools/Heatmap.mouseTSS.H3K9ac.pdf --averageType mean --perGroup --colorMap RdBu --averageTypeSummaryPlot mean --legendLocation upper-right --heatmapWidth 12
+plotHeatmap -m Deeptools/mouseTSS.H3K9ac.tab.gz --regionsLabel "Mouse Refeq Genes" --plotFileFormat "pdf" --samplesLabel "KO" "KO" "WT" "WT" "WT" "KOInput" "KOInput" "WTInput" "WTInput" "WTInput" "WTInput" "WTInput" -out Deeptools/Heatmap.mouseTSS.H3K9ac.pdf --averageType mean --perGroup --colorMap bwr --averageTypeSummaryPlot mean --legendLocation upper-right --heatmapWidth 12
 
 ############################################################################################
